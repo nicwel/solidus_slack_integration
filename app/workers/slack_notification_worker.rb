e09db48bd9b253fcea30c0b48287506eaef9e1fa
nicwel <nico.weling@googlemail.com>
2020-09-05 16:01:55 +0200

    Updated to slack-notifier >2.x

--- app/workers/slack_notification_worker.rb
+++ app/workers/slack_notification_worker.rb
@@ -5,7 +5,10 @@ class SlackNotificationWorker
 
   def perform(order_number)
     order = Spree::Order.find_by_number(order_number)
-    notifier = Slack::Notifier.new(ENV['SLACK_TEAM_NAME'], ENV['SLACK_CHANNEL_TOKEN'])
-    notifier.ping(order.slack_notification_message, channel: ENV['SLACK_CHANNEL_NAME'])
+    #notifier = Slack::Notifier.new "https://hooks.slack.com/services/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", channel: "orders", username: "notifier"
+    notifier = Slack::Notifier.new ENV['SLACK_WEBHOOK_URL'], channel: ENV['SLACK_ORDER_CHANNEL'], username: "rails-notifier"
+    notifier.ping order.slack_notification_message
+    #notifier = Slack::Notifier.new(ENV['SLACK_TEAM_NAME'], ENV['SLACK_CHANNEL_TOKEN'])
+    #notifier.ping(order.slack_notification_message, channel: ENV['SLACK_CHANNEL_NAME'])
   end
 end
