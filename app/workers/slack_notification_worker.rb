25ddb7082bbb3748c31b0aa7b34365c6ddc70fcb
nicwel <nico.weling@googlemail.com>
2020-09-07 17:10:26 +0200

    Catch error if order==nil

--- app/workers/slack_notification_worker.rb
+++ app/workers/slack_notification_worker.rb
@@ -6,6 +6,10 @@ class SlackNotificationWorker
   def perform(order_number)
     order = Spree::Order.find_by_number(order_number)
     notifier = Slack::Notifier.new ENV['SLACK_WEBHOOK_URL'], channel: ENV['SLACK_ORDER_CHANNEL'], username: "rails-notifier"
-    notifier.ping "Received order: #{order_number} #{order.state} #{order.email} #{order.name} #{order.amount}" # order.slack_notification_message
+    if order # avoid order.state called on nil error
+      notifier.ping "Received order: #{order_number} #{order.state} #{order.email} #{order.name} #{order.amount}" # order.slack_notification_message
+    else
+      notifier.ping "Received order: #{order_number}"
+    end
   end
 end
